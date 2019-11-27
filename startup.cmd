5791d7486ab21207ddb0ce5e864737450897899e
fuqunaga <fukhid@gmail.com>
2019-11-27 18:28:35 +0900

    don't touch extension

--- startup.cmd
+++ startup.cmd
@@ -13,13 +13,13 @@ if %Minimize%==TRUE (
 timeout %DelayTime%
 
 for %%* in (.) do set appname=%%~nx*
-set logfile=%appname%_Data\output_log.txt
+set logfile=%appname%_Data\output_log
 SETLOCAL enabledelayedexpansion
 for /L %%i in (10,-1,1) do (
 		call set /A j=%%i+1
-		if exist %logfile%_%%i move /Y %logfile%_%%i %logfile%_!j!
+		if exist %logfile%_%%i.txt move /Y %logfile%_%%i.txt %logfile%_!j!.txt
 		)
-if exist %logfile% move /Y %logfile% %logfile%_1
+if exist %logfile%.txt move /Y %logfile%.txt %logfile%_1.txt
 ENDLOCAL
  
 if %KillExplorer%==TRUE ( taskkill /f /IM explorer.exe )
